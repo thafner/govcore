b4b6d79b199f60e8cb0ef9680fc2ce4a02617703
Robert Shell <r.shell@claritypartners.com>
2021-11-21 23:39:47 -0600

    missing module

---
+++ tarball.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ARCHIVE=govcore-$1
+
+composer create-project --stability beta --no-install drupal/legacy-project:~9.1.0 $ARCHIVE
+composer dump-autoload
+composer configure-tarball $ARCHIVE
+
+cd $ARCHIVE
+composer config minimum-stability dev
+composer config prefer-stable true
+composer config repositories.assets composer https://asset-packagist.org
+composer remove --no-update composer/installers
+composer require --no-update "ext-dom:*" "acquia/lightning:~5.1.0" cweagans/composer-patches
+composer update
+
+# Add the version number to the info file.
+echo "version: $1" >> ./profiles/contrib/govcore/govcore.info.yml
+
+# Wrap it all up in a nice compressed tarball.
+cd ..
+tar --exclude='.DS_Store' --exclude='._*' -c -z -f $ARCHIVE.tar.gz $ARCHIVE
+
+# Clean up.
+rm -r -f $ARCHIVE
