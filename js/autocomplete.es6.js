cffb8a0b5e63708ea99f0b3f27b9a4021c6ae611
Robert Shell <r.shell@claritypartners.com>
2022-06-03 13:12:10 -0500

    css and js

---
+++ js/autocomplete.es6.js
@@ -0,0 +1,42 @@
+/**
+ * @file
+ * Cook County Admin's enhancement for autocomplete form element.
+ */
+
+(($, Drupal, once) => {
+  Drupal.behaviors.cook_county_adminAutoCompete = {
+    attach(context) {
+      once('cook_county_adminAutoComplete', 'input.form-autocomplete', context).forEach(
+        (value) => {
+          const $input = $(value);
+          const timeout = 400;
+          let classRemoveTimeout;
+          const classRemove = ($autoCompleteElem) => {
+            $autoCompleteElem.removeClass('is-autocompleting');
+            $autoCompleteElem
+              .siblings('[data-drupal-selector="autocomplete-message"]')
+              .addClass('hidden');
+          };
+
+          $input.on(
+            'input autocompletesearch autocompleteresponses',
+            (event) => {
+              if (event && event.type && event.type === 'autocompletesearch') {
+                $(event.target).addClass('is-autocompleting');
+                $(event.target)
+                  .siblings('[data-drupal-selector="autocomplete-message"]')
+                  .removeClass('hidden');
+              }
+              clearTimeout(classRemoveTimeout);
+              classRemoveTimeout = setTimeout(
+                classRemove,
+                timeout,
+                $(event.target),
+              );
+            },
+          );
+        },
+      );
+    },
+  };
+})(jQuery, Drupal, once);
